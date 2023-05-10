17db110c1a8f8969e37b7c1687aeceb8a3f313fa
moong <moonki0623@naver.com>
2023-05-10 22:14:14 +0900

    feat: navbar 완료

--- components/navbar/navbar.element.js
+++ components/navbar/navbar.element.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 export const NavbarContainer = styled.nav`
   position: fixed;
   bottom: 0;
-  left: 0;
-  width: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: max(46.18vh, 24.438rem);
   height: 60px;
   background-color: #000;
   z-index: 999;
@@ -13,4 +14,37 @@ export const NavbarContainer = styled.nav`
   justify-content: space-around;
   align-items: center;
   color: white;
+  ${(props) => (props.render ? "" : "display: none;")}
+  & > div {
+    flex-basis: 18%;
+    margin: 0 1%;
+  }
+`;
+
+export const NavbarItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & > svg {
+    color: ${(props) => (props.active ? "white" : "#8C8787")};
+  }
+  & > svg > path {
+    fill: ${(props) => (props.active ? "white" : "#8C8787")};
+  }
+  & > span {
+    font-size: 0.5125rem;
+    margin-top: 0.175rem;
+    color: ${(props) => (props.active ? "white" : "#8C8787")};
+  }
+  &:hover {
+    cursor: pointer;
+    & > svg {
+      transform: scale(1.4);
+      transition: all 0.2s ease-in-out;
+    }
+    & > span {
+      display: none;
+    }
+  }
 `;
--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,9 +1,36 @@
-const { NavbarContainer } = require("./navbar.element");
+// const { NavbarContainer, NavbarItem } = require("./navbar.element");
+import { NavbarContainer, NavbarItem } from "./navbar.element";
+import { BiHomeAlt2, BiSearch, BiMenu } from "react-icons/bi";
+import { HiDownload } from "react-icons/hi";
+import { FiMenu } from "react-icons/fi";
+import Cs from "../../public/cs";
+import { useRouter } from "next/router";
 
-const Navbar = () => {
+const Navbar = ({ render }) => {
+  const router = useRouter();
+  console.log(router.pathname);
   return (
-    <NavbarContainer>
-      <>Navbar</>
+    <NavbarContainer render={true}>
+      <NavbarItem active={router.pathname === "/"}>
+        <BiHomeAlt2 size={24} />
+        <span>Home</span>
+      </NavbarItem>
+      <NavbarItem active={router.pathname === "/search"}>
+        <BiSearch size={24} />
+        <span>Search</span>
+      </NavbarItem>
+      <NavbarItem active={router.pathname === "/scheduled"}>
+        <Cs color={"gray"} />
+        <span>Coming Soon</span>
+      </NavbarItem>
+      <NavbarItem active={router.pathname === "/download"}>
+        <HiDownload size={24} />
+        <span>Downloads</span>
+      </NavbarItem>
+      <NavbarItem active={router.pathname === "/more"}>
+        <FiMenu size={24} />
+        <span>More</span>
+      </NavbarItem>
     </NavbarContainer>
   );
 };
