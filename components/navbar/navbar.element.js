bb1d51a3196b014dbe1b5cf21e73a42b2f532600
moong <moonki0623@naver.com>
2023-05-10 16:16:46 +0900

    initializing project: directory 구조 세팅 및 styledcomponent 세팅

---
+++ components/navbar/navbar.element.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const NavbarContainer = styled.nav`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #000;
+  z-index: 999;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
---
+++ components/navbar/navbar.js
@@ -0,0 +1,11 @@
+const { NavbarContainer } = require("./navbar.element");
+
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+      <>Navbar</>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
