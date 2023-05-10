d9962bac16a0b43668a0b39e6741944aea4d8b5d
moong <moonki0623@naver.com>
2023-05-10 23:13:40 +0900

    feat: genreList itself content

--- components/genreList/genreList.js
+++ components/genreList/genreList.js
@@ -1,9 +1,49 @@
-import { GenreListContainer } from "./genreList.element";
+import Image from "next/image";
+import {
+  GenreListContainer,
+  GenreListContent,
+  GenreListContentItem,
+  GenreListTitle,
+} from "./genreList.element";
 
-const GenreList = () => {
+const GenreList = ({ type, name }) => {
   return (
-    <GenreListContainer>
-      <>GL</>
+    <GenreListContainer type={type}>
+      <GenreListTitle type={type}>{name}</GenreListTitle>
+      <GenreListContent>
+        <GenreListContentItem type={type}>
+          <Image
+            src="/dall-e-1.png"
+            alt="img"
+            width={100}
+            height={type === 1 ? 100 : 175}
+          />
+        </GenreListContentItem>
+        <GenreListContentItem type={type}>
+          <Image
+            src="/dall-e-1.png"
+            alt="img"
+            width={100}
+            height={type === 1 ? 100 : 175}
+          />
+        </GenreListContentItem>
+        <GenreListContentItem type={type}>
+          <Image
+            src="/dall-e-1.png"
+            alt="img"
+            width={100}
+            height={type === 1 ? 100 : 175}
+          />
+        </GenreListContentItem>
+        <GenreListContentItem type={type}>
+          <Image
+            src="/dall-e-1.png"
+            alt="img"
+            width={100}
+            height={type === 1 ? 100 : 175}
+          />
+        </GenreListContentItem>
+      </GenreListContent>
     </GenreListContainer>
   );
 };
