f6766ce9906baad9ad092b11d67d66f4702f4661
moong <moonki0623@naver.com>
2023-05-10 23:19:43 +0900

    initialize: initializing landing_page structure and branch

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,14 @@
 import "@/styles/globals.css";
+import { RecoilRoot } from "recoil";
 import Navbar from "../../components/navbar/navbar";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <RecoilRoot>
       <div className="mainContainer">
         <Component {...pageProps} />
       </div>
       <Navbar />
-    </>
+    </RecoilRoot>
   );
 }
