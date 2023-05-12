7ae08ac8dca7722ff2916ab4bb7fd05f7aa628c3
moong <moonki0623@naver.com>
2023-05-12 11:53:42 +0900

    WIP: scroll function adding in progress

--- components/header/header.js
+++ components/header/header.js
@@ -1,13 +1,30 @@
 import { HeaderWrapper, Icon, Tab } from "./header.element";
+import {
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  useLayoutEffect,
+} from "react";
 
 const Header = () => {
+  const layoutRef = useRef(null);
+  function handleScroll() {
+    console.log("scrolled!");
+  }
+
+  useEffect(() => {
+    document.body.addEventListener("scroll", handleScroll);
+    return () => document.body.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const headerData = [
     { idx: 1, name: "TV Shows" },
     { idx: 2, name: "Movies" },
     { idx: 3, name: "My List" },
   ];
   return (
-    <HeaderWrapper>
+    <HeaderWrapper ref={layoutRef}>
       <Icon
         src="/icon-logo.png"
         alt="logo"
