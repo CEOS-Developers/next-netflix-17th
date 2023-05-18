2fdf2493ed4288bb2153cd63ccccaf6a038fbebd
hyosin <>
2023-05-19 05:20:28 +0900

    feat: react-query 세팅

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,25 @@
 import "@/styles/globals.css";
 import { RecoilRoot } from "recoil";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import Navbar from "../../components/navbar/navbar";
+import Layout from "../../components/layout";
 
 export default function App({ Component, pageProps }) {
+  // useState lazy init을 사용해 QueryClient 인스턴스 생성
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <RecoilRoot>
-      <div className="mainContainer">
-        <Component {...pageProps} />
-      </div>
-      <Navbar />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Layout>
+        <Navbar />
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
