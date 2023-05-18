ddc42a90cf0d48c5e5ce0ca02fe768b7994e8ca9
hyosin <>
2023-05-19 05:27:19 +0900

    fix: 불필요한 스타일 제거

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -12,12 +12,3 @@ export const flexCenter = css`
   justify-content: center;
   align-items: center;
 `;
-
-const customMediaQuery = (maxWidth) => `@media (max-width: ${maxWidth}px)`;
-
-export const media = {
-  custom: customMediaQuery,
-  pc: customMediaQuery(1440),
-  tablet: customMediaQuery(768),
-  mobile: customMediaQuery(576),
-};
