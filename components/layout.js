3b0646cccfc4c3e88ae6f5deb365ea0f93b21a89
hyosin <>
2023-05-19 14:25:39 +0900

    chore: layout head 태그 추가

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,18 @@
-const Layout = ({ children }) => {
-  return <div className="mainContainer">{children}</div>;
+import Head from "next/head";
+
+const Layout = ({ title, description, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content={description}
+        />
+      </Head>
+      <div className="mainContainer">{children}</div>
+    </>
+  );
 };
 
 export default Layout;
