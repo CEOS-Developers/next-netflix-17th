cd53782d9cb06b8511e79a2c7f1d81752753f254
moong <moonki0623@naver.com>
2023-05-19 17:58:18 +0900

    [feat]: skeleton structure added to movieData and mainContent

--- components/MovieData/movieData.js
+++ components/MovieData/movieData.js
@@ -2,11 +2,23 @@ const {
   MovieDataContainer,
   MovieDataTitle,
   MovieDataContent,
+  MovieDataTitleFB,
+  MovieDataContentFB,
 } = require("./movieData.element");
 
 // 상위 컴포넌트인 [id].js에서 호출됨
-// data: 서버에서 받아온 데이터 영화 제목 / 청불 여부 / 줄거리 / 개봉일
-const MovieData = ({ data }) => {
+// data: 서버에서 받아온 데이터 영화 제목 / 청불 여부 / 줄거리
+const MovieData = ({ data, fallback }) => {
+  if (fallback) {
+    return (
+      <MovieDataContainer>
+        <MovieDataTitleFB />
+        <MovieDataContentFB />
+        <MovieDataContentFB />
+        <MovieDataContentFB />
+      </MovieDataContainer>
+    );
+  }
   return (
     <MovieDataContainer>
       <MovieDataTitle adult={data.adult ? 1 : 0}>{data.title}</MovieDataTitle>
